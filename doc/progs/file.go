e75467c8494fefd24923c9da4aa6ff04c1ab0143
chaishushan <chaishushan@1dd72658-072c-11df-8e78-e7f5014194b1>
2010-01-26 02:46:07 +0000

    add go_tutorial_zh.txt
    git-svn-id: https://golang-china.googlecode.com/svn/trunk@2 1dd72658-072c-11df-8e78-e7f5014194b1

---
+++ doc/progs/file.go
@@ -0,0 +1,74 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package file
+
+import (
+	"os"
+	"syscall"
+)
+
+type File struct {
+	fd      int    // file descriptor number
+	name    string // file name at Open time
+}
+
+func newFile(fd int, name string) *File {
+	if fd < 0 {
+		return nil
+	}
+	return &File{fd, name}
+}
+
+var (
+	Stdin  = newFile(0, "/dev/stdin")
+	Stdout = newFile(1, "/dev/stdout")
+	Stderr = newFile(2, "/dev/stderr")
+)
+
+func Open(name string, mode int, perm int) (file *File, err os.Error) {
+	r, e := syscall.Open(name, mode, perm)
+	if e != 0 {
+		err = os.Errno(e)
+	}
+	return newFile(r, name), err
+}
+
+func (file *File) Close() os.Error {
+	if file == nil {
+		return os.EINVAL
+	}
+	e := syscall.Close(file.fd)
+	file.fd = -1  // so it can't be closed again
+	if e != 0 {
+		return os.Errno(e)
+	}
+	return nil
+}
+
+func (file *File) Read(b []byte) (ret int, err os.Error) {
+	if file == nil {
+		return -1, os.EINVAL
+	}
+	r, e := syscall.Read(file.fd, b)
+	if e != 0 {
+		err = os.Errno(e)
+	}
+	return int(r), err
+}
+
+func (file *File) Write(b []byte) (ret int, err os.Error) {
+	if file == nil {
+		return -1, os.EINVAL
+	}
+	r, e := syscall.Write(file.fd, b)
+	if e != 0 {
+		err = os.Errno(e)
+	}
+	return int(r), err
+}
+
+func (file *File) String() string {
+	return file.name
+}
---
+++ doc/progs/helloworld3.go
@@ -0,0 +1,21 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"./file"
+	"fmt"
+	"os"
+)
+
+func main() {
+	hello := []byte{'h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '\n'}
+	file.Stdout.Write(hello)
+	file, err := file.Open("/does/not/exist",  0,  0)
+	if file == nil {
+		fmt.Printf("can't open file; err=%s\n",  err.String())
+		os.Exit(1)
+	}
+}
